1cb51cb12ecf457904665f4f89be2f26d599b63e
nkakouros <tterranigma@gmail.com>
2021-02-15 14:32:47 +0100

    Add space between words

--- src/oai_spgwu/main.cpp
+++ src/oai_spgwu/main.cpp
@@ -105,7 +105,7 @@ int main(int argc, char** argv) {
   // Checking if another instance of SPGW-U is running
   int nb_processes = my_check_redundant_process(argv[0]);
   if (nb_processes > 1) {
-    std::cout << "An instance of " << argv[0] << "is maybe already called!"
+    std::cout << "An instance of " << argv[0] << " is maybe already called!"
               << std::endl;
     return -1;
   }
