7f687f853eaa7617ba56da186d0d55afb6219558
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2021-07-08 16:43:28 +0200

    CI: triggering the CN5G FED testing pipeline on the Ubuntu18 image
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- ci-scripts/generateHtmlReport.py
+++ ci-scripts/generateHtmlReport.py
@@ -163,7 +163,7 @@ class HtmlReport():
 		self.file.write(buildSummary)
 
 		cwd = os.getcwd()
-		test_reports = ['test_results_oai_epc', 'test_results_magma_epc_rhel8']
+		test_reports = ['test_results_oai_epc', 'test_results_magma_epc_rhel8', 'test_results_oai_cn5g']
 		for test in test_reports:
 			if os.path.isfile(cwd + '/' + test + '.html'):
 				newEpcReport = open(cwd + '/' + test + '_new.html', 'w')
